a63b09ceee703e600727a5f10f789b97ed93c5c3
triptiagarwal-05 <triptiagarwal.gzb@gmail.com>
2025-11-02 18:35:35 +0530

    Create App.js

---
+++ Experiment 9.2/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🚀 GitHub Actions CI/CD Pipeline Demo</h1>
+      <p>This React app was automatically built and deployed!</p>
+    </div>
+  );
+}
+
+export default App;
