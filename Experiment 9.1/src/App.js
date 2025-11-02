ee52b9102bf2d9b36cf1a22e3ed762ac1eacc026
triptiagarwal-05 <triptiagarwal.gzb@gmail.com>
2025-11-02 18:30:56 +0530

    Create App.js

---
+++ Experiment 9.1/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>🚀 React App Dockerized Successfully!</h1>
+      <p>This React app is running inside a Docker container using a multi-stage build.</p>
+    </div>
+  );
+}
+
+export default App;
